05ac085929682cd7a360ac741703938ee5105d8d
Sultanov Nurzhan <s8n6a6@gmail.com>
2025-01-31 22:21:00 +0500

    Fixed all bugs

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+declare module "*.module.css" {
+    const classes: { [key: string]: string };
+    export default classes;
+}
+interface ImportMetaEnv {
+    readonly VITE_WEATHER_API_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
